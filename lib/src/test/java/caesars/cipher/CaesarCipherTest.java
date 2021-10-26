a9322046ae5811741a590ce6822ffff46e91e505
millikr2 <59751763+millikr2@users.noreply.github.com>
2021-10-26 14:38:06 -0400

    Added a new test

--- lib/src/test/java/caesars/cipher/CaesarCipherTest.java
+++ lib/src/test/java/caesars/cipher/CaesarCipherTest.java
@@ -12,11 +12,16 @@ class CaesarCipherTest {
 	void testCipheredMessageWithOffsetOf12() {
 		assertEquals("tai mdq kag pauzs fapmk", caesarCipher.cipher("how are you doing today", 12));
 	}
+	@Test
+	void testCipheredMessageWithOffsetOf11() {
+		assertEquals("tai mdq kag pauzs fapmk", caesarCipher.cipher("how are you doing today", 11));
+	}
 	
 	@Test 
 	void testEmptyString() {
 		assertEquals("", caesarCipher.cipher("", 12));
 	}
+
 
 
 
